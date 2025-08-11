b07e8ba6ed844c4c58cb77271b4b1cc776a4d546
Ashish <ashishbastola2002@gmail.com>
2025-08-11 17:30:55 +0530

    added div in page

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -8,6 +8,9 @@ import { useBalance } from "@repo/store/balance";
 export default function() {
   const balance = useBalance();
   return <div>
+    <div>
+      hi
+    </div>
     hi there {balance}
   </div>
 }
