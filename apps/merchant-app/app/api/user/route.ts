076a7e08ba0fc1953bf2836f4cab31dd1764e59d
Ashish <ashishbastola2002@gmail.com>
2025-08-11 18:59:33 +0530

    fixed bug

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -2,14 +2,16 @@ import { NextResponse } from "next/server"
 import  prisma  from "@repo/db/client";
 
 export const GET = async () => {
-    await prisma.user.create({
-        data: {
-            email: "asd",
-            name: "adsads",
-            number:"123456",
-            password:"90900"
-        }
-    })
+    await prisma.user.upsert({
+    where: { email: "asd" },
+    update: {},
+    create: {
+      email: "asd",
+      name: "adsads",
+      password: "secret",
+      number: "123",
+    },
+  });
     return NextResponse.json({
         message: "hi there"
     })
